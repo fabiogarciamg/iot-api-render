c9c8082e99c7c9e8dad70b5bb951b427bc026221
fabiogarciamg <fabiogarciamg@gmail.com>
2025-10-09 16:35:13 -0300

    Update package.js

--- package.js
+++ package.js
@@ -1,12 +1,15 @@
 {
-  "name": "iot-api-render",
+  "name": "iot-api",
   "version": "1.0.0",
-  "main": "server.js",
+  "description": "API IoT para receber dados do ESP32 e salvar no MongoDB Atlas",
+  "main": "index.js",
+  "type": "module",
   "scripts": {
-    "start": "node server.js"
+    "start": "node index.js"
   },
   "dependencies": {
     "express": "^4.18.2",
-    "mongodb": "^5.7.0"
+    "mongodb": "^5.7.0",
+    "dotenv": "^16.3.1"
   }
 }
